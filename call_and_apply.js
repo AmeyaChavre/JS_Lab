1963ac0e626ae7c657c608fed52bb960160a11c1
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-16 17:30:39 +0530

    Commit#23

--- call_and_apply.js
+++ call_and_apply.js
@@ -6,11 +6,19 @@ const car1 = {
   }
 }
 
-car1.getDescription(200000,2003,'black');
+
 
 const car2 = {
   manufacturer: 'Koenigsegg ',
-  model:'One',
+  model:'One'
 }
 
 car1.getDescription.call(car2,700000,2016,'grey');
+
+const car3 = {
+	manufacturer: 'Pagani',
+	model:'Zonda'
+}
+car1.getDescription(200000,2003,'black');
+car1.getDescription.call(car2,700000,2016,'grey');
+car1.getDescription.apply(car3,[800000,2018,'black']);
