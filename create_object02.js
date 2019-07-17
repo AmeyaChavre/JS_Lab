dcc6953edaca17df6b1c02dcbec8e439620dd8b8
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-17 20:38:17 +0530

    Commit#32

---
+++ create_object02.js
@@ -0,0 +1,15 @@
+//@Method1
+
+const formulaOneObj =
+{
+maxSpeed:434,
+maxRPM:7000,
+maxHP:2300,
+getmaxSpeed: function()
+{
+  return this.maxSpeed;
+}
+};
+
+
+console.log(formulaOneObj.getmaxSpeed());
