8d4c6cd60f5939ee2f01e00d59da1c79b810ff1d
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-19 21:41:38 +0530

    Commit#84

---
+++ spaceTravelBind.js
@@ -0,0 +1,13 @@
+// @ Lets Bind()
+
+// bind is used to attach this context to a function
+
+
+const spaceTravel = {
+distance : 4000,
+getDistance : function(){
+  return this.distance;
+}
+};
+
+console.log(spaceTravel);
