8a3152e6678bcef72c2a085485be23f50f39ebb4
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-19 21:04:48 +0530

    Commit#67

--- dataTypes.js
+++ dataTypes.js
@@ -4,7 +4,7 @@
 console.log(typeof(null)); // null is of type object
 console.log(typeof(undefined)); // undefined is of the undefined
 console.log(typeof({})); // function is of the type object
-console.log(typeof([]));
+console.log(typeof([])); // array is of the type object
 
 /*@Output
 object
