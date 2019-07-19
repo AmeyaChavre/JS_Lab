cf4026a445d33f7a9bd96da66fac49c9b64725bf
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-19 22:03:16 +0530

    Commit#91

--- spaceTravelBind.js
+++ spaceTravelBind.js
@@ -10,18 +10,12 @@ getDistance : function(){
 }
 };
 
-const spaceTravel_1 = {
-distance : 8000,
-getDistance : function(){
-  return this.distance;
-}
-};
 
-// console.log(spaceTravel);
+const tripToMoon = spaceTravel.getDistance();
 
-const tripToMoon = spaceTravel.getDistance().bind(spaceTravel_1);
 
-console.log(tripToMoon);
+ 
+
 
 
 
