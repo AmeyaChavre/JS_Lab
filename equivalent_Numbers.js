bf3e1c933a87250ed9791a05a57208a212abd084
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-21 22:52:54 +0530

    Commit#185

---
+++ equivalent_Numbers.js
@@ -0,0 +1,13 @@
+console.log(900.9 === (300.3 * 3));
+
+var a = 900.0;
+var b = 300.3 * 3;
+console.log(a);
+console.log(b);
+console.log(a===b); 
+/* Output :
+false
+900
+900.9000000000001
+false
+*/
