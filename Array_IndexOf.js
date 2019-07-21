495e627c294352fbeff601add4e57811d57de105
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-21 12:24:56 +0530

    Commit#180

--- Array_IndexOf.js
+++ Array_IndexOf.js
@@ -1,7 +1,7 @@
 console.log([10, 20, 30, 40, 50].indexOf(30));
-console.log([{ name: 'Pam' }, { name: 'Kent' }].indexOf({ name: 'Kent' }));
+console.log([{ name: 'Pam' }, { name: 'Kent' }].indexOf({ name: 'Kent' })); // not found 
 console.log('hello world'.indexOf('o'));
-console.log([[1], [2], [3], [4]].indexOf([2]));
+console.log([[1], [2], [3], [4]].indexOf([2])); // not found
 
 
 /*
