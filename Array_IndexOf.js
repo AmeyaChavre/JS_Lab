37b9d86e339afdf66800d71da581f7ae5ae096ea
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-21 12:28:15 +0530

    Commit#181

--- Array_IndexOf.js
+++ Array_IndexOf.js
@@ -1,7 +1,7 @@
 console.log([10, 20, 30, 40, 50].indexOf(30));
-console.log([{ name: 'Pam' }, { name: 'Kent' }].indexOf({ name: 'Kent' })); // not found 
+console.log([{ name: 'Pam' }, { name: 'Kent' }].indexOf({ name: 'Kent' })); // not found as two these are two different objects
 console.log('hello world'.indexOf('o'));
-console.log([[1], [2], [3], [4]].indexOf([2])); // not found
+console.log([[1], [2], [3], [4]].indexOf([2])); // not found as these are two different arrays
 
 
 /*
