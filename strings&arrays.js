b946a36a6b34809bb462d3ad7d8db7f44e1940d9
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-21 23:57:42 +0530

    Commit#189

---
+++ strings&arrays.js
@@ -0,0 +1,11 @@
+const data1 = 'Jordan Smith';
+ 
+const data2 = [].filter.call(data1, function(elem, index) {
+  return index > 6;
+});
+ 
+console.log(data2);
+
+/* Output:
+[ 'S', 'm', 'i', 't', 'h' ]
+*/
