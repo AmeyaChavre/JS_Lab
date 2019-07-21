5a396c9044cc560a48efffbba21f05719e02ecac
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-21 12:34:38 +0530

    Commit#182

--- Array_IndexOf.js
+++ Array_IndexOf.js
@@ -1,7 +1,9 @@
+const myArray = [5];
+const anotherArray = myArray;
 console.log([10, 20, 30, 40, 50].indexOf(30));
 console.log([{ name: 'Pam' }, { name: 'Kent' }].indexOf({ name: 'Kent' })); // not found as two these are two different objects
 console.log('hello world'.indexOf('o'));
-console.log([[1], [2], [3], [4]].indexOf([2])); // not found as these are two different arrays
+console.log([[1], [2], [3], [4],anotherArray].indexOf(anotherArray)); // not found as these are two different arrays
 
 
 /*
