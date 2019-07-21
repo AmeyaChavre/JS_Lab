860f6dc690621d6caa2a1528938aed9412fc9a21
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-22 00:31:42 +0530

    Commit#190

---
+++ objProperty.js
@@ -0,0 +1,16 @@
+const a = {};
+const b = { name : 'b' };
+const c = { name : 'c' };
+
+a[b] = 200;
+// a['[object Object]'] = 200;
+a[c] = 400;
+//  a['[object Object]'] = 400;
+
+console.log(a); // { '[object Object]': 400 }
+console.log(a[b]); // 400
+
+/*Output
+{ '[object Object]': 400 }
+400
+*/
