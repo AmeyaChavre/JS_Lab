59db1c58e810bded2724bbceae6fb85d7e20bcd1
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-25 11:23:55 +0530

    Commit#204

--- isPalindrome_A05.js
+++ isPalindrome_A05.js
@@ -3,7 +3,7 @@ function isPalindrome(string)
 console.log("Input String: ",string);
 console.log("Input String Length: ",string.length);
 var stringLength = string.length;
-
+var str = '';
 for (var i = stringLength; i >= 0; i--)
 {
 	
