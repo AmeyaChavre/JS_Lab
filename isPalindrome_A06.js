d4a72ce897c7b03c768fc8fb09d7e2d63b1245f0
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-26 19:00:08 +0530

    Commit#225

---
+++ isPalindrome_A06.js
@@ -0,0 +1,26 @@
+// An improvement of isPalindrome_A05.js to handle whitespaces and small and capital letters
+
+function isPalindrome(str1)
+{
+var str2 = str1.toLowerCase();
+var str3 = str2.split(" ").join("");
+console.log("Processed Input String : ",str3);
+var str3_len = str3.length;
+console.log("Input String Length : ",str3_len);
+var str4=""
+for (var i = str3_len-1; i >= 0; i--)
+{
+  str4+=str3[i];
+}
+console.log("Processed Output String : ",str4);
+if (str4===str3)
+{
+  return true;
+}
+else
+{
+  return false;
+}
+}
+//isPalindrome("Never odd or even")
+isPalindrome('stonecold');
