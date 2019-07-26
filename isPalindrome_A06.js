4d7a2d52afeee8bbcf00759cc0eb51f66497a725
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-26 19:01:23 +0530

    Commit#229

--- isPalindrome_A06.js
+++ isPalindrome_A06.js
@@ -6,3 +6,4 @@ var str2 = str1.toLowerCase();
 var str3 = str2.split(" ").join("");
 console.log("Processed Input String : ",str3);
 var str3_len = str3.length;
+console.log("Input String Length : ",str3_len);
