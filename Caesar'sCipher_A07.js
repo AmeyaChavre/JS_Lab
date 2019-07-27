63b1de12b582df1724a59101d65bf59cbd82021b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-27 12:23:56 +0530

    Commit#248

--- Caesar'sCipher_A07.js
+++ Caesar'sCipher_A07.js
@@ -2,7 +2,22 @@
 
 function caesarCipher(str,num)
 {
+var lowerCaseString = str.toLowerCase();
+//console.log(lowerCaseString);
+var alphabetArr = lowerCaseString.split('');
+//console.log(alphabetArr);
+var newString = '';
+for (var i = 0; i < lowerCaseString.length; ++i)
+{
+var currentAlphabet = lowerCaseString[i];
+if (currentAlphabet === ' ')
+{
+  newString += currentAlphabet;
+}
+if 
 
 }
 
-function caesarCipher();
+}
+
+caesarCipher('Aa Bb Cc Dd',1);
