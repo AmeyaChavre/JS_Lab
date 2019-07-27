06a17fde7bcf49395658e47414319413dcd9ed13
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-27 13:17:50 +0530

    Commit#257

--- reverseWord_A08.js
+++ reverseWord_A08.js
@@ -1,7 +1,25 @@
 function reverseString(string)
 {
 
-console.log(string);
+stringArr = string.split(' ');
+//console.log(stringArr);
+var newString = '';
+for (var i = 0; i < stringArr.length; ++i )
+{
+  for(var j = stringArr[i].length; j>=0 ; --j)
+  {
+    if(string[j]===' ')
+    {
+      newString+=string[j];
+      continue;
+    }
+  else
+    {
+    newString+=string[j];
+    }
+  }
+}
+console.log(newString);
 }
 
 reverseString('Hello World');
