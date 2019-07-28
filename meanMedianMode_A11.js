cdc2e2681c40d92df013778f0563ccb858e5fc9e
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-28 10:39:34 +0530

    Commit#265

---
+++ meanMedianMode_A11.js
@@ -0,0 +1,19 @@
+function meanMediaMode(array){
+  return {
+    mean:getMean(),
+    median:getMedian(),
+    mode:getMode()
+  }
+}
+// Mean
+function getMean(array){
+
+}
+// Median
+function getMedian(array){
+
+}
+// Mode
+function getMode(array){
+
+}
