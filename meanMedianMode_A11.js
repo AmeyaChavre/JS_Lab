ea3615236bdd945b65745f114a13d4f754f7c5f4
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-28 10:58:48 +0530

    Commit#268

--- meanMedianMode_A11.js
+++ meanMedianMode_A11.js
@@ -20,7 +20,18 @@ return mean;
 // Median
 
 function getMedian(array){
+array.sort(function(a,b) return {a-b};)
+var median;
 
+if(array.length % 2 !== 0){
+median = array[Math.floor(array.length/2)];
+}
+else{
+var mid1 = array[(array.length/2)-1];
+var mid2 = array[array.length/2];
+median = (mid1+mid2)/2;
+}
+return median;
 }
 
 // Mode
