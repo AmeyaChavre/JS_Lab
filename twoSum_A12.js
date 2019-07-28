33fd502a1b65cbec02274921e2886c603930bf1d
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-28 11:30:08 +0530

    Commit#275

--- twoSum_A12.js
+++ twoSum_A12.js
@@ -2,12 +2,11 @@ function twoSum(numArray ,sum){
 var pairs = [];
 var hashTable = [];
 
-for (var i = 0; i < numArray.length; ++i)
-{
+for (var i = 0; i < numArray.length; ++i){
 var currNum = numArray[i];
 var counterPart = sum - currNum;
 if (hashTable.indexOf(counterPart) !== -1){
-	
+	pairs.push([currNum,counterPart]);
 }	
 
 }
