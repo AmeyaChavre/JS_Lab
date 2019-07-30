27fc958e9ee88325770454c7c9709a3e809b4f81
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-30 12:20:28 +0530

    Commit#287

---
+++ bubbleSort_A17.js
@@ -0,0 +1,26 @@
+function bubbleSort(array){
+for (var i = array.length; i > 0; --i)
+  {
+    for (var j = 0; j < i; j++)
+    {
+      if(array[j] > array[j+1])
+      {
+        var temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+return array;
+
+}
+
+bubbleSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 9, 1]);
+
+
+/* @Output:
+
+=> [ 1, 3, 9, 34, 198, 200, 203, 746, 764, 984, 6000 ]
+
+
+*/
