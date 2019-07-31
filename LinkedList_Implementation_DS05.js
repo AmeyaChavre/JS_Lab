fd39302215dc6dcb1904423b51b50d595713da3a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-31 21:50:25 +0530

    Commit#310

--- LinkedList_Implementation_DS05.js
+++ LinkedList_Implementation_DS05.js
@@ -46,47 +46,34 @@ console.log(ll);
 ll.addToTail(300);
 console.log(ll);
 
-
 /* @Output:
 
-function LinkedList(){
-this.head = null;
-this.tail = null;
+LinkedList { head: null, tail: null }
+LinkedList {
+  head: Node { value: 100, next: null, prev: null },
+  tail: Node { value: 100, next: null, prev: null } }
+LinkedList {
+  head:
+   Node {
+     value: 100,
+     next: Node { value: 200, next: null, prev: [Circular] },
+     prev: null },
+  tail:
+   Node {
+     value: 200,
+     next: null,
+     prev: Node { value: 100, next: [Circular], prev: null } } }
+LinkedList {
+  head:
+   Node {
+     value: 100,
+     next: Node { value: 200, next: [Node], prev: [Circular] },
+     prev: null },
+  tail:
+   Node {
+     value: 300,
+     next: null,
+     prev: Node { value: 200, next: [Circular], prev: [Node] } } }
 
-}
 
-
-function Node(value,next,prev){
-this.value = value;
-this.next = next;
-this.prev = prev;
-}
-
-
-// Add to the head method implementation
-
-LinkedList.prototype.addToHead = function(value){
-	// creating a new node
-var newNode = new Node(value, this.head, null); 
-	// if nodes already exist
-if(this.head) this.head.prev = newNode;
-else this.tail = newNode; // if no nodes exist
-this.head = newNode;
-}
-
-// Add to the tail method implementation
-
-LinkedList.prototype.addToTail = function(value){
-	// creating a new node 
-var newNode = new Node(value, null, this.tail);
-// as this is the last node and nodes exist
-if(this.tail) this.tail.next = newNode;
-else this.head = newNode;
-this.tail = newNode;
-}
-
-var ll = LinkedList();
-
-console.log(ll);
-
-*/
+     */
