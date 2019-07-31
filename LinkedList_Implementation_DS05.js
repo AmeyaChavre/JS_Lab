445916b5ee2bf16ec83e0d4c347980478722efaf
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-31 21:44:19 +0530

    Commit#309

--- LinkedList_Implementation_DS05.js
+++ LinkedList_Implementation_DS05.js
@@ -33,3 +33,60 @@ if(this.tail) this.tail.next = newNode;
 else this.head = newNode;
 this.tail = newNode;
 }
+
+var ll = new LinkedList();
+
+console.log(ll);
+
+
+ll.addToTail(100);
+console.log(ll);
+ll.addToTail(200);
+console.log(ll);
+ll.addToTail(300);
+console.log(ll);
+
+
+/* @Output:
+
+function LinkedList(){
+this.head = null;
+this.tail = null;
+
+}
+
+
+function Node(value,next,prev){
+this.value = value;
+this.next = next;
+this.prev = prev;
+}
+
+
+// Add to the head method implementation
+
+LinkedList.prototype.addToHead = function(value){
+	// creating a new node
+var newNode = new Node(value, this.head, null); 
+	// if nodes already exist
+if(this.head) this.head.prev = newNode;
+else this.tail = newNode; // if no nodes exist
+this.head = newNode;
+}
+
+// Add to the tail method implementation
+
+LinkedList.prototype.addToTail = function(value){
+	// creating a new node 
+var newNode = new Node(value, null, this.tail);
+// as this is the last node and nodes exist
+if(this.tail) this.tail.next = newNode;
+else this.head = newNode;
+this.tail = newNode;
+}
+
+var ll = LinkedList();
+
+console.log(ll);
+
+*/
