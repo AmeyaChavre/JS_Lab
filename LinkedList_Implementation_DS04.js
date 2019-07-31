7ca1f79ffdfcf1cf742265f02fc75c7f646ef485
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-31 21:10:06 +0530

    Commit#306

--- LinkedList_Implementation_DS04.js
+++ LinkedList_Implementation_DS04.js
@@ -24,3 +24,28 @@ this.head = newNode;
 }
 
 
+var ll = new LinkedList();
+
+ll.addToHead(100); // tail node >> "last node"
+ll.addToHead(200); // intermediate node >> "second node"
+ll.addToHead(300); // head node >> "first node"
+
+console.log(ll);
+
+
+/* @Output:
+
+LinkedList {
+  head:
+   Node {
+     value: 300,
+     next: Node { value: 200, next: [Node], prev: [Circular] },
+     prev: null },
+  tail:
+   Node {
+     value: 100,
+     next: null,
+     prev: Node { value: 200, next: [Circular], prev: [Node] } } }
+
+     */
+
