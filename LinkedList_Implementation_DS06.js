208e085135a519e2bc06d14c99d629a07f117920
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-31 22:28:22 +0530

    Commit#314

--- LinkedList_Implementation_DS06.js
+++ LinkedList_Implementation_DS06.js
@@ -33,3 +33,58 @@ if(this.tail) this.tail.next = newNode;
 else this.head = newNode;
 this.tail = newNode;
 }
+
+// Remove head function implementation
+
+LinkedList.prototype.removeHead = function(){
+  if(!this.head) return null; // linked list is already empty
+  var val = this.head.value;
+  this.head = this.head.next;
+  if(this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
+}
+
+
+var ll = new LinkedList(); // creating a new linked list 
+
+ll.addToHead(100);
+ll.addToHead(50);
+ll.addToTail(150);
+ll.addToTail(200);
+
+
+
+console.log(ll.removeHead()); // returns 50
+console.log(ll.head); // returns 100
+console.log(ll.head.next); // returns 150
+console.log(ll.head.next.next); // returns 200
+console.log(ll.head.next.next.next); // returns null as linked list is empty
+
+
+/* @Output:
+
+50
+Node {
+  value: 100,
+  next:
+   Node {
+     value: 150,
+     next: Node { value: 200, next: null, prev: [Circular] },
+     prev: [Circular] },
+  prev: null }
+Node {
+  value: 150,
+  next: Node { value: 200, next: null, prev: [Circular] },
+  prev: Node { value: 100, next: [Circular], prev: null } }
+Node {
+  value: 200,
+  next: null,
+  prev:
+   Node {
+     value: 150,
+     next: [Circular],
+     prev: Node { value: 100, next: [Circular], prev: null } } }
+null
+
+*/
