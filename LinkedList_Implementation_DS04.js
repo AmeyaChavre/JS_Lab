839279dec5ba43c6f0aff3ac3c118e8077b209be
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-07-31 20:11:02 +0530

    Commit#302

---
+++ LinkedList_Implementation_DS04.js
@@ -0,0 +1,15 @@
+function LinkedList(){
+this.head = null;
+this.tail = null;
+
+}
+
+
+function Node(value,next,prev){
+this.value = value;
+this.next = next;
+this.prev = prev;
+}
+
+
+// Add to the head method implementation
