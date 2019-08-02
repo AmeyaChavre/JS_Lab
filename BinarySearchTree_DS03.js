a72741ff18a6a960e43301e625a96dffbf38736a
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-02 12:50:39 +0530

    Commit#376

--- BinarySearchTree_DS03.js
+++ BinarySearchTree_DS03.js
@@ -1,6 +1,6 @@
 /* Binary Search Tree Implementation */
 
-// Binary Search Tree Constructor Function
+// Binary Search Tree Constructor Function (Root Node , no left or right child nodes)
 function BST(value){
 this.value = value;
 this.left = null;
@@ -10,3 +10,9 @@ this.right = null;
 var bt1 = new BST(50);
 
 console.log(bt1);
+
+/* @Output:
+
+BST { value: 50, left: null, right: null }
+
+*/
