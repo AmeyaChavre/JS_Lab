f833d641209545fb9f235157fa6e4acf869e4952
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-02 12:29:15 +0530

    Commit#343

--- BinarySearchTree_DS02.js
+++ BinarySearchTree_DS02.js
@@ -4,16 +4,16 @@
 /* Recursion example */
 
 
-function calculateFactorial(num){
+function factorial(num){
 	if(num===1){
 		return 1;
 	}
 	else{
-		return calculateFactorial(num) * calculateFactorial(num-1);
+		return num * factorial(num-1);
 	}
 	
 }
 
 
 
-calculateFactorial(4); // Output: 4! = 4 * 3 * 2 * 1 = 24
+factorial(4); // Output: 4! = 4 * 3 * 2 * 1 = 24
