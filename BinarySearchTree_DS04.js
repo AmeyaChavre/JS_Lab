2bd798a6fe9c860c53d1b56254df70ebcad0442b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-02 13:04:32 +0530

    Commit#377

---
+++ BinarySearchTree_DS04.js
@@ -0,0 +1,27 @@
+/* Binary Search Tree Implementation */
+
+// Binary Search Tree Constructor Function (Root Node , no left or right child nodes)
+function BST(value){
+this.value = value;
+this.left = null;
+this.right = null;
+}
+
+// Implementing a Insert Functionality
+
+BST.prototype.insert = function(value){
+	if(value <= this.value){
+		if(!this.left) this.left = new BST(value);
+		else this.left.insert(value);
+	}
+	else if(value > this.value){
+		if(!this.right) this.right = new BST(value);
+		else this.right.insert(value);
+	}	
+}
+
+
+
+
+var bt1 = new BST(50);
+
