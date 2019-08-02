b3ac8bd1def8bdf77fd9034d789519853f9f572d
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-02 12:16:11 +0530

    Commit#338

--- BinarySearchTree_DS01.js
+++ BinarySearchTree_DS01.js
@@ -1,3 +1,17 @@
 /* Binary Search Tree Implementation */
 
 
+function someFunc(value){
+	if(/*base case*/)
+	{
+		console.log("Some Statement");
+	}
+	else
+	{
+		someFunc(value); // recursion with some termination condition involved
+	}
+	
+	
+}
+
+// this code will not run ; to be used further in BST Implementation
