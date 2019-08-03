6c063ab6a3782121f38d0755379fe63f54e41696
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-03 21:53:55 +0530

    Commit#421

--- BinarySearchTree_DS05.js
+++ BinarySearchTree_DS05.js
@@ -47,7 +47,7 @@ bst1.insert(); // insert a value
 bst1.insert(); // insert a value
 bst1.insert(); // insert a value
 bst1.insert(); // insert a value
-bst1.insert();
+bst1.insert(); // insert a value
 bst1.insert();
 bst1.insert();
 bst1.insert();
