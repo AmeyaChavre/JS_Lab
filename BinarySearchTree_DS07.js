a469119425660568e9f847f86e17ad42028168ca
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-04 00:05:17 +0530

    Commit#468

--- BinarySearchTree_DS07.js
+++ BinarySearchTree_DS07.js
@@ -46,7 +46,7 @@ BST.prototype.contains = function(value){
 BST.prototype.depthFirstTraversal = function(iteratorFunc,order){
 		if(this.left) this.left.depthFirstTraversal(iteratorFunc,order);
 		iteratorFunc(this.value);
-		if(this.right) this.right.depthFirstTraversal(iteratorFunc);
+		if(this.right) this.right.depthFirstTraversal(iteratorFunc,order);
 }
 
 var bst1 = new BST(50); // root node
