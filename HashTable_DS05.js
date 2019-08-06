6f96ec535e5b448108cfbd7ad413033d852e51d9
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-06 18:47:59 +0530

    Commit#615

---
+++ HashTable_DS05.js
@@ -0,0 +1,43 @@
+/* Hash Table - Implementation */
+
+// Hash Table Constructor Function
+
+function HashTable(size){
+	this.buckets = Array(size);
+	this.numBuckets = this.buckets.length; 
+}
+
+// Hash Node Constructor Function
+
+function HashNode(key,value,next){
+	this.key = key;
+	this.value = value;
+	this.next = next || null;
+}
+
+// Implementing a Hash Method
+
+HashTable.prototype.hash = function(key){
+	var total = 0;
+	for(var i = 0; i < key.length; ++i){
+		total += key.charCodeAt(i);
+	}
+	var bucket = total % this.numBuckets;
+	return bucket;
+}
+
+// Implementing Insert Method => takes a {key:value} pair => turn them into hash node , place them in the correct bucket
+
+// Implementing a Refactored version of the insert method to be able to update the email address
+
+
+var myHT = new HashTable(30);
+myHT.insert('Chuck Berry','chuckberry@gmail.com');
+myHT.insert('Stevie Wonder','steviewonder@gmail.com');
+myHT.insert('Elvis Presly','elvispresly@gmail.com');
+myHT.insert('Freddy Mercury','freddymercury@gmail.com');
+
+
+
+
+console.log(myHT.buckets);
