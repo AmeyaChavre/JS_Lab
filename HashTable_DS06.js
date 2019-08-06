9969c106be63fc168528f4c90f747665ff4c3c03
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-06 19:54:08 +0530

    Commit#706

--- HashTable_DS06.js
+++ HashTable_DS06.js
@@ -84,7 +84,7 @@ myHT.insert('Freddy Mercury','freddymercury.1980@gmail.com');
 // Testing Get Method 
 
 console.log('Singer1 Email Address: ',myHT.get('Chuck Berry'));
-console.log('Singer1 Email Address: ',myHT.get());
+console.log('Singer1 Email Address: ',myHT.get('Stevie Wonder'));
 console.log('Singer1 Email Address: ',myHT.get());
 console.log('Singer1 Email Address: ',myHT.get());
 console.log('Singer1 Email Address: ',myHT.get());
