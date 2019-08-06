930cb5a01c62cc72f4ddb0b1f3c24e07fba3d245
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-06 18:53:10 +0530

    Commit#625

--- HashTable_DS05.js
+++ HashTable_DS05.js
@@ -33,7 +33,7 @@ HashTable.prototype.hash = function(key){
 HashTable.prototype.insert = function(){
 	var index = this.hash(key);
 	if(!this.buckets[index]){
-		this.buckets[index] = new HashNode();
+		this.buckets[index] = new HashNode(key);
 	}
 	else{
 		
