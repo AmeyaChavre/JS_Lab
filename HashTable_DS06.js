4e464b3ea8e4dbdaf21956985e506c30294655ab
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-08-06 19:53:32 +0530

    Commit#703

--- HashTable_DS06.js
+++ HashTable_DS06.js
@@ -89,5 +89,5 @@ console.log('Singer1 Email Address: ',myHT.get());
 console.log('Singer1 Email Address: ',myHT.get());
 console.log('Singer1 Email Address: ',myHT.get());
 console.log('Singer1 Email Address: ',myHT.get());
-console.log('Singer1 Email Address: ',);
+console.log('Singer1 Email Address: ',myHT.get());
 console.log('Singer1 Email Address: ',);
